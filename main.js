2f2c924138170281ac46e31403c3a666888a2425
Zack Birkenbuel <github@badkungfu.com>
2024-11-09 19:40:03 -0800

    Switch to classlist instead of stomping on styles, less likely to be removed later by other frameworks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const PROCESSED = Symbol("walker processed");
 
 const TEXT_REPLACEMENTS = [
   {re: /Donald Trump/gi, to: 'Orange Man'},
-  {re: /Trump/gi, to: 'Orange Man'},
+  {re: /Trump/gi, to: 'Orange Man'}, // maybe strumpet.
   {re: /Elon Musk/gi, to: 'Some Guy'},
   {re: /Elon/gi, to: 'Some Guy'},
   {re: /Musk/gi, to: 'Some Guy'},
@@ -10,7 +10,7 @@ const TEXT_REPLACEMENTS = [
   {re: /Kennedy/gi, to: 'Wormtail'},
   {re: /JD Vance/gi, to: 'Sofa King'},
   {re: /J\.D\. Vance/gi, to: 'Sofa King'},
-  {re: /\WVance\W/gi, to: 'Sofa King'},
+  {re: /Vance/gi, to: 'Sofa King'}, // maybe advancement
   {re: /Make America Great Again/gi, to: 'Cult Membership'},
 ]
 
@@ -18,9 +18,9 @@ const TEXT_REPLACEMENTS = [
 function performTextHole(text) {
   let txt = text || "";
   for (const replacement of TEXT_REPLACEMENTS) {
-    text = text.replaceAll(replacement.re, replacement.to);
+    txt = txt.replaceAll(replacement.re, replacement.to);
   }
-  return text;
+  return txt;
 }
 
 function shouldHole(text) {
@@ -32,7 +32,7 @@ function performImageHole(img) {
   if (img[PROCESSED]) {
     return;
   }
-  img.setAttribute("style", "filter: blur(1em);");
+  img.classList.add("memoryholed");
   img[PROCESSED] = true;
 
 }
@@ -46,21 +46,25 @@ function holeSimilarLinks(global, node) {
       // find all of the similar links
       const similar = global.document.querySelectorAll(`a[href="${href}"]`);
       for (const sim of similar) {
-        // make the link not work
+        if (sim[PROCESSED]) {
+          continue;
+        }
+
+        sim[PROCESSED] = true;
         sim.setAttribute("data-original-href", href);
         sim.setAttribute("href", "#");
-        // Mayyyybe there are images that weren't caught previously, blur them too?
-        // Be better
-        for (const img of sim.querySelectorAll("img")) {
+        // Images that are within links but weren't identified before.
+        // Be better, use alt text people!
+        const imgs = sim.querySelectorAll("img");
+        for (const img of imgs) {
           performImageHole(img);
         }
-        sim[PROCESSED] = true;
       }
     }
   }
 }
 
-async function performMemoryHole(global) {
+function performMemoryHole(global) {
   // walk everything looking for the thing to memory hole
   const textWalker = global.document.createTreeWalker(global.document, NodeFilter.SHOW_TEXT, {
     acceptNode(node) {
@@ -86,26 +90,27 @@ async function performMemoryHole(global) {
   const imgWalker = global.document.createTreeWalker(global.document, NodeFilter.SHOW_ELEMENT, {
     acceptNode(node) {
       if (node[PROCESSED]) {
+        // skip Just this node, still process its children.
         return NodeFilter.FILTER_REJECT;
       }
       // TODO, support for picture and figure?
       if (node.nodeName.toLowerCase() !== "img") {
         return NodeFilter.FILTER_SKIP
       }
-      // Alt text for sure since news sources should alt text everything.  Maybe SRCs too?
       // TODO: support for source sets.
+      // This will only ever really match single word regexs, but better than nothing.
       if (shouldHole(node.getAttribute("src"))) {
         return NodeFilter.FILTER_ACCEPT;
       }
+      // Alt text for sure since news sources should alt text everything.
       return shouldHole(node.getAttribute("alt")) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_REJECT;
     }
   });
   
-  // TODD: Veeery similar to the above, so is the walker for that matter, could maybe refactor.
+  // TODO: Veeery similar to the above, so is the walker for that matter, could maybe refactor?
   while (imgWalker.nextNode()) {
     const node = imgWalker.currentNode;
     // mark the node as processed so it isn't scanned again
-    console.log(node);
     node[PROCESSED] = true;
     // replace the image
     performImageHole(node);
@@ -116,17 +121,22 @@ async function performMemoryHole(global) {
 
 
 // Actually initialize
-(async (global) => {
+((global) => {
   // watch the whole dom for node changes that might contain the text.
   const options = {
     subtree: true,
     childList: true,
   }
-  const observer = new MutationObserver(async (mutationList) => {
-    // could probably just target the things that mutated, but for now hamfistedly walk the whole tree.
-    await performMemoryHole(global);
+  const observer = new MutationObserver((mutationList) => {
+    // schedule this to happen on the next tick since it appears the mutation observer doesn't
+    // seem to be capturing some final image additions on bsky
+    setTimeout(() => {
+      // could probably just target the things that mutated, but for now hamfistedly re-walk the whole tree.
+      performMemoryHole(global);
+    }, 0);
   });
   observer.observe(global.document, options);
   
-  await performMemoryHole(global);
+  // just perform an initial pass for essentially static sites.
+  performMemoryHole(global);
 })(globalThis);
